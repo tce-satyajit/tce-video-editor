26741a0a3e4e649f48600978cad38c41d5f1820a
satyajitp <satyajitp@tataclassedge.com>
2024-11-02 23:42:55 +0530

    updated

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # VideoEditor
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.0.
 
+Issue
+https://stackoverflow.com/questions/78964764/error-loading-ffmpeg-referenceerror-sharedarraybuffer-is-not-defined-angular
 
+Mac
+open -na "Google Chrome" --args --enable-features=SharedArrayBuffer --disable-web-security --user-data-dir=/tmp/tempChrome
+
+Win
+chrome.exe --enable-features=SharedArrayBuffer --disable-web-security --user-data-dir="C:/ChromeDev"
--- server.js
+++ server.js
@@ -5,25 +5,26 @@ const path = require('path');
 const app = express();
 
 // Set the necessary headers
+/*
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
-
+*/
 // Serve static files from Angular dist folder
-app.use(express.static(path.join(__dirname, 'dist/video-editor')));
+//app.use(express.static(path.join(__dirname, 'dist/video-editor')));
 
 // Proxy API requests if needed
 app.use(
   '/api',
   createProxyMiddleware({
-    target: 'http://localhost:6600',
+    target: 'http://localhost:6800',
     changeOrigin: true,
   })
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
